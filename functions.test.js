c666e3d7bced4612bf31cbb7f1bb8d6fd9d5a6a3
Gregory Daynes <gregdaynes@gmail.com>
2019-12-01 17:09:44 -0800

    Solve puzzle 02
    A module has a mass, that requires a certain amount of fuel. The fuel
    for that module also has a mass that requires fuel.
    
    Implementing a pair of functions that recursively calculate the fuel
    required for the fuel required for a module. Then like puzzle 01,
    utilize a reducer to sum up the fuel mass required.

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,7 @@
 const {
   calcFuelFor,
+  calcFuelForInclusive,
+  calcTotalFuelFor,
 } = require('./functions');
 
 describe("#calcFuelFor", () => {
@@ -11,5 +13,29 @@ describe("#calcFuelFor", () => {
   test("a mass of 8 requires a fuel of 0", () => {
     const subject = calcFuelFor(7);
     expect(subject).toBe(0);
+  });
+});
+
+describe("#calcFuelForInclusive", () => {
+  test("a 1969 mass requires an additional 966 mass of fuel", () => {
+    const subject = calcFuelForInclusive(1969);
+    expect(subject).toBe(1969 + 966);
+  });
+
+  test("a 100756 mass requires an additional 50346 mass of fuel", () => {
+    const subject = calcFuelForInclusive(100756);
+    expect(subject).toBe(100756 + 50346);
+  });
+});
+
+describe("#calcFuelForMass", () => {
+  test("a 1969 mass requires 966 mass of fuel", () => {
+    const subject = calcTotalFuelFor(1969);
+    expect(subject).toBe(966);
+  });
+
+  test("a 100756 mass requires 50346 mass of fuel", () => {
+    const subject = calcTotalFuelFor(100756);
+    expect(subject).toBe(50346);
   });
 });
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const {
   calcFuelFor,
+  calcFuelForInclusive,
+  calcTotalFuelFor,
 } = require('./functions');
 
 const input = fs.readFileSync('./input.data', 'utf8')
@@ -12,3 +14,11 @@ const answerP1 = input
     return acc + calcFuelFor(parseInt(mass));
   }, 0);
 console.log('Answer D01-P01', answerP1);
+
+const answerP2 = input
+  .trim()
+  .split("\n")
+  .reduce((acc, mass) => {
+    return acc + calcTotalFuelFor(parseInt(mass))
+  }, 0);
+console.log('Answer D01-P02', answerP2);
