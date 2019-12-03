aaa83d0474c897e21839e4e33e6c29078756e14f
Gregory Daynes <gregdaynes@gmail.com>
2019-12-02 19:37:44 -0800

    Solve Day 02 - Part 2
    Uses a rudementary 2D for loop ot iterate over the range 0-99 twice.
    Each number in the iteration is inserted into a copy of the intcode and
    then computed. Successful computation occurs when the output of compute
    matches the value in the specification.
    
    The specification asks what the 2 loop values found are in the formula
    100 * noun + verb.

--- day02.js
+++ day02.js
@@ -21,4 +21,31 @@ function puzzle1() {
 
   return compute(newIntcode)[0];
 }
-console.log(puzzle1(), puzzle1() === 3166704);
+const answerP1 = puzzle1();
+console.log('Answer D02-P01', answerP1, answerP1 === 3166704);
+
+function puzzle2() {
+  const input = fs.readFileSync('./day02.data', 'utf8');
+
+  let foundNoun;
+  let foundVerb;
+
+  for (let noun = 0; noun < 100; noun += 1) {
+    for (let verb = 0; verb < 100; verb += 1) {
+      const tempInput = input.slice(0);
+      const newIntcode = replaceIntcodePoints(tempInput, [[1, noun], [2, verb]]);
+      const output = compute(newIntcode)[0];
+
+      if (output === 19690720) {
+        foundNoun = noun;
+        foundVerb = verb;
+        break;
+      }
+    }
+  }
+
+  return 100 * foundNoun + foundVerb;
+}
+const answerP2 = puzzle2();
+console.log('Answer D02-P02', answerP2, answerP2 === 8018);
+
