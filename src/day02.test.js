1f8b0a8d40239e2d77ce1d8493a3181fc2c85f71
Gregory Daynes <gregdaynes@gmail.com>
2019-12-02 21:11:13 -0800

    Add structure and tests for Day 02

---
+++ src/day02.test.js
@@ -0,0 +1,11 @@
+const { part1, part2 } = require('./day02');
+
+describe('Day 02', () => {
+  it('Part 1', () => {
+    expect(part1()).toBe(3166704);
+  });
+
+  it('Part 2', () => {
+    expect(part2()).toBe(8018);
+  });
+});
